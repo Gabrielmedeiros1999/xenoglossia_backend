fd0f0f08822ce9303b97f7a77adc1a2d915d5db0
Gabriel Caldeira Medeiros <gabrielcaldeiramedeiros1999@gmail.com>
2026-05-06 13:02:16 -0300

    melhorias no backend

--- backend.py
+++ backend.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
-from routes import router, IDIOMAS, IDIOMAS_PT
+from routes import router, IDIOMAS_PT
 from routes_auth import router_auth
-from deep_translator import GoogleTranslator
 import json
 import os
 
@@ -11,35 +10,31 @@ app = FastAPI(title="Tradutor API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-def traduzir_idiomas():
-    if os.path.exists("idiomas_pt.json"):
-        with open("idiomas_pt.json", "r", encoding="utf-8") as f:
-            IDIOMAS_PT.update(json.load(f))
-        return
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CAMINHO_JSON = os.path.join(BASE_DIR, "idiomas_pt.json")
 
-    for nome_en, codigo in IDIOMAS.items():
-        try:
-            nome_pt = GoogleTranslator(source='en', target='pt').translate(nome_en)
-            nome_pt = nome_pt.title()
-        except:
-            nome_pt = nome_en.title()
 
-        IDIOMAS_PT[nome_pt] = codigo
+def carregar_idiomas():
+    if not os.path.exists(CAMINHO_JSON):
+        raise FileNotFoundError("❌ idiomas_pt.json não encontrado no backend")
 
-    # salva no arquivo
-    with open("idiomas_pt.json", "w", encoding="utf-8") as f:
-        json.dump(IDIOMAS_PT, f, ensure_ascii=False, indent=2)
+    with open(CAMINHO_JSON, "r", encoding="utf-8") as f:
+        IDIOMAS_PT.update(json.load(f))
+
+    print("✅ Idiomas carregados com sucesso")
+
 
 @app.on_event("startup")
 def startup():
     Base.metadata.create_all(bind=engine)
-    traduzir_idiomas() 
+    carregar_idiomas()
+
 
 app.include_router(router)
 app.include_router(router_auth)
